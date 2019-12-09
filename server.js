c30b9dc541ca8a681dc55f2037d618ccbe95f97d
Mattia Mertens <mattianc@hotmail.it>
2019-12-09 12:33:18 +0100

    Nodemailer

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+// var mailTo = document.getElementById('mail');
+
 // Nodemailer
 var nodemailer = require('nodemailer');
+var hbs = require('nodemailer-express-handlebars');
+const handlebarOptions = {
+    viewEngine: {
+      extName: '.hbs',
+      partialsDir: './views/partials',
+      layoutsDir: './views/layouts',
+      defaultLayout: 'index.hbs',
+    },
+    viewPath: './views/',
+    extName: '.hbs',
+};
 
 // Transporter
 var transporter = nodemailer.createTransport({
@@ -10,27 +23,23 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+// Handlebars
+transporter.use('compile', hbs(handlebarOptions));
+
 // Mail options
 var mailOptions = {
     from: 'trigger@gmail.com',
     to: 'gurki4.mm@gmail.com',
-    subject: 'test',
+    subject: 'test con immagini + click',
     text: 'AAA',
-    attachments: [{
-        filename: 'gianbibi.jpg', path: 'savedPNG/gianbibi.jpg'}
-    ]
+    // attachments: [{
+    //     filename: 'gianbibi.jpg', path: 'views/gianbibi.jpg'}
+    // ],
+    template: 'index'
 };
 
 //Send Mail
-transporter.sendMail(mailOptions, function(err, data){
-    if (err){
-        console.log('fuck no');
-    }
-    else{
-        console.log('yes daje');
-    }
-    
-});
+
 
 
 
@@ -50,17 +59,29 @@ io.sockets.on('connection', newConnection);
 function newConnection(socket){
     console.log('ciao: ' + socket.id);
 
+    // Email sender function
+
+
     // Calcio 1
     socket.on('co_1', co_1);
     function co_1(data){
         console.log('oopop');
         io.sockets.emit('r_co_1');
         // socket.broadcast.emit('return-msg', data);
+        transporter.sendMail(mailOptions, function(err, data){
+            if (err){
+                console.log('fuck no' + err);
+            }
+            else{
+                console.log('yes daje');
+                alert('yes daje');
+            }            
+        });
     }
+
     // Calcio 2
     socket.on('co_2', co_2);
     function co_2(data){
         io.sockets.emit('r_co_2');
-        // socket.broadcast.emit('return-msg', data);
     }
 };
