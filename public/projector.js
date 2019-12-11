c2044e43cf9b968a9ba81463c07ed024be06d491
Mattia Mertens <mattianc@hotmail.it>
2019-12-11 22:30:39 +0100

    fino a grilletto tutto ok

--- public/projector.js
+++ public/projector.js
@@ -5,19 +5,50 @@ var mail_trigger = document.getElementsByClassName('email')[0]; // send email vi
 var message = document.getElementsByClassName('done')[0]; // non mi ricordo a cosa serva
 
 // VAR for sockets CALCIO
-var v_co_1 = document.getElementsByClassName('magenta')[0];
-var v_co_2 = document.getElementsByClassName('calx')[0];
-var v_co_3 = document.getElementsByClassName('lime')[0];
-var v_co_4 = document.getElementsByClassName('extramarine')[0];
-var v_co_5 = document.getElementsByClassName('obsidian')[0];
-var v_co_6 = document.getElementsByClassName('white')[0];
-var v_co_7 = document.getElementsByClassName('orange')[0];
-var v_co_8 = document.getElementsByClassName('fuck')[0];
+var v_co_1 = document.getElementsByClassName('magenta')[0],
+    v_ca_1 = document.getElementsByClassName('magenta')[1],
+    v_gr_1 = document.getElementsByClassName('magenta')[2],
+    v_de_1 = document.getElementsByClassName('magenta')[3];
+
+var v_co_2 = document.getElementsByClassName('calx')[0],
+    v_ca_2 = document.getElementsByClassName('calx')[1],
+    v_gr_2 = document.getElementsByClassName('calx')[2],
+    v_de_2 = document.getElementsByClassName('calx')[3];
+
+var v_co_3 = document.getElementsByClassName('lime')[0],
+    v_ca_3 = document.getElementsByClassName('lime')[1],
+    v_gr_3 = document.getElementsByClassName('lime')[2],
+    v_de_3 = document.getElementsByClassName('lime')[3];
+
+var v_co_4 = document.getElementsByClassName('extramarine')[0],
+    v_ca_4 = document.getElementsByClassName('extramarine')[1],
+    v_gr_4 = document.getElementsByClassName('extramarine')[2],
+    v_de_4 = document.getElementsByClassName('extramarine')[3];
+
+var v_co_5 = document.getElementsByClassName('obsidian')[0],
+    v_ca_5 = document.getElementsByClassName('obsidian')[1],
+    v_gr_5 = document.getElementsByClassName('obsidian')[2],
+    v_de_5 = document.getElementsByClassName('obsidian')[3];
+
+var v_co_6 = document.getElementsByClassName('white')[0],
+    v_ca_6 = document.getElementsByClassName('white')[1],
+    v_gr_6 = document.getElementsByClassName('white')[2],
+    v_de_6 = document.getElementsByClassName('white')[3];
+
+var v_co_7 = document.getElementsByClassName('orange')[0],
+    v_ca_7 = document.getElementsByClassName('orange')[1],
+    v_gr_7 = document.getElementsByClassName('orange')[2],
+    v_de_7 = document.getElementsByClassName('orange')[3];
+
+var v_co_8 = document.getElementsByClassName('fuck')[0],
+    v_ca_8 = document.getElementsByClassName('fuck')[1],
+    v_gr_8 = document.getElementsByClassName('fuck')[2],
+    v_de_8 = document.getElementsByClassName('fuck')[3];
+
 
 // send email via form
 mail_trigger.addEventListener('click', function(){
-    socket.emit('mail_sender', {
-    });
+    socket.emit('mail_sender', {});
 });
 
 
@@ -54,6 +85,76 @@ v_co_8.addEventListener('click', function(){
     socket.emit('co_8', {
     });
 });
+
+// Socket for canna
+v_ca_1.addEventListener('click', function(){
+    socket.emit('ca_1', {
+    });
+});
+v_ca_2.addEventListener('click', function(){
+    socket.emit('ca_2', {
+    });
+});
+v_ca_3.addEventListener('click', function(){
+    socket.emit('ca_3', {
+    });
+});
+v_ca_4.addEventListener('click', function(){
+    socket.emit('ca_4', {
+    });
+});
+v_ca_5.addEventListener('click', function(){
+    socket.emit('ca_5', {
+    });
+});
+v_ca_6.addEventListener('click', function(){
+    socket.emit('ca_6', {
+    });
+});
+v_ca_7.addEventListener('click', function(){
+    socket.emit('ca_7', {
+    });
+});
+v_ca_8.addEventListener('click', function(){
+    socket.emit('ca_8', {
+    });
+});
+
+
+// Socket for grilletto
+v_gr_1.addEventListener('click', function(){
+    socket.emit('gr_1', {
+    });
+});
+v_gr_2.addEventListener('click', function(){
+    socket.emit('gr_2', {
+    });
+});
+v_gr_3.addEventListener('click', function(){
+    socket.emit('gr_3', {
+    });
+});
+v_gr_4.addEventListener('click', function(){
+    socket.emit('gr_4', {
+    });
+});
+v_gr_5.addEventListener('click', function(){
+    socket.emit('gr_5', {
+    });
+});
+v_gr_6.addEventListener('click', function(){
+    socket.emit('gr_6', {
+    });
+});
+v_gr_7.addEventListener('click', function(){
+    socket.emit('gr_7', {
+    });
+});
+v_gr_8.addEventListener('click', function(){
+    socket.emit('gr_8', {
+    });
+});
+
 
 
 // LISTENER FOR EVENTS
@@ -119,4 +220,131 @@ function co_8(data){
     console.log('calcio 8');
     $('.fuck_co').addClass('projected');
     $('#calcio').children('div').not('.fuck_co').removeClass('projected'); // remove Class from other parts
+};
+
+
+//Canna 1
+socket.on('r_ca_1', ca_1);
+function ca_1(data){
+    console.log('canna 1');
+    $('.magenta_ca').addClass('projected');
+    $('#canna').children('div').not('.magenta_ca').removeClass('projected'); // remove Class from other parts
+};
+//Canna 2
+socket.on('r_ca_2', ca_2);
+function ca_2(data){
+    console.log('canna 2');
+    $('.calx_ca').addClass('projected');
+    $('#canna').children('div').not('.calx_ca').removeClass('projected'); // remove Class from other parts
+};
+//Canna 3
+socket.on('r_ca_3', ca_3);
+function ca_3(data){
+    console.log('canna 3');
+    $('.lime_ca').addClass('projected');
+    $('#canna').children('div').not('.lime_ca').removeClass('projected'); // remove Class from other parts
+};
+
+//Canna 4
+socket.on('r_ca_4', ca_4);
+function ca_4(data){
+    console.log('canna 4');
+    $('.extramarine_ca').addClass('projected');
+    $('#canna').children('div').not('.extramarine_ca').removeClass('projected'); // remove Class from other parts
+};
+
+//Canna 5
+socket.on('r_ca_5', ca_5);
+function ca_5(data){
+    console.log('canna 5');
+    $('.obsidian_ca').addClass('projected');
+    $('#canna').children('div').not('.obsidian_ca').removeClass('projected'); // remove Class from other parts
+};
+
+//Canna 6
+socket.on('r_ca_6', ca_6);
+function ca_6(data){
+    console.log('canna 6');
+    $('.white_ca').addClass('projected');
+    $('#canna').children('div').not('.white_ca').removeClass('projected'); // remove Class from other parts
+};
+
+//Canna 7
+socket.on('r_ca_7', ca_7);
+function ca_7(data){
+    console.log('canna 7');
+    $('.orange_ca').addClass('projected');
+    $('#canna').children('div').not('.orange_ca').removeClass('projected'); // remove Class from other parts
+};
+
+//Canna 8
+socket.on('r_ca_8', ca_8);
+function ca_8(data){
+    console.log('canna 8');
+    $('.fuck_ca').addClass('projected');
+    $('#canna').children('div').not('.fuck_ca').removeClass('projected'); // remove Class from other parts
+};
+
+
+
+//grilletto 1
+socket.on('r_gr_1', gr_1);
+function gr_1(data){
+    console.log('grilletto 1');
+    $('.magenta_gr').addClass('projected');
+    $('#grilletto').children('div').not('.magenta_gr').removeClass('projected'); // remove Class from other parts
+};
+//grilletto 2
+socket.on('r_gr_2', gr_2);
+function gr_2(data){
+    console.log('grilletto 2');
+    $('.calx_gr').addClass('projected');
+    $('#grilletto').children('div').not('.calx_gr').removeClass('projected'); // remove Class from other parts
+};
+//grilletto 3
+socket.on('r_gr_3', gr_3);
+function gr_3(data){
+    console.log('grilletto 3');
+    $('.lime_gr').addClass('projected');
+    $('#grilletto').children('div').not('.lime_gr').removeClass('projected'); // remove Class from other parts
+};
+
+//grilletto 4
+socket.on('r_gr_4', gr_4);
+function gr_4(data){
+    console.log('grilletto 4');
+    $('.extramarine_gr').addClass('projected');
+    $('#grilletto').children('div').not('.extramarine_gr').removeClass('projected'); // remove Class from other parts
+};
+
+//grilletto 5
+socket.on('r_gr_5', gr_5);
+function gr_5(data){
+    console.log('grilletto 5');
+    $('.obsidian_gr').addClass('projected');
+    $('#grilletto').children('div').not('.obsidian_gr').removeClass('projected'); // remove Class from other parts
+};
+
+//grilletto 6
+socket.on('r_gr_6', gr_6);
+function gr_6(data){
+    console.log('grilletto 6');
+    $('.white_gr').addClass('projected');
+    $('#grilletto').children('div').not('.white_gr').removeClass('projected'); // remove Class from other parts
+};
+
+//grilletto 7
+socket.on('r_gr_7', gr_7);
+function gr_7(data){
+    console.log('grilletto 7');
+    $('.orange_gr').addClass('projected');
+    $('#grilletto').children('div').not('.orange_gr').removeClass('projected'); // remove Class from other parts
+};
+
+//grilletto 8
+socket.on('r_gr_8', gr_8);
+function gr_8(data){
+    console.log('grilletto 8');
+    $('.fuck_gr').addClass('projected');
+    $('#grilletto').children('div').not('.fuck_gr').removeClass('projected'); // remove Class from other parts
 };
--- server.js
+++ server.js
@@ -81,19 +81,16 @@ function newConnection(socket){
         io.sockets.emit('r_co_1');
         // socket.broadcast.emit('return-msg', data);
     }
-
     // Calcio 2
     socket.on('co_2', co_2);
     function co_2(data){
         io.sockets.emit('r_co_2');
     }
-
     // Calcio 3
     socket.on('co_3', co_3);
     function co_3(data){
         io.sockets.emit('r_co_3');
     }
-
     // Calcio 4
     socket.on('co_4', co_4);
     function co_4(data){
@@ -118,5 +115,89 @@ function newConnection(socket){
     socket.on('co_8', co_8);
     function co_8(data){
         io.sockets.emit('r_co_8');
+    }
+
+
+    // Canna 1
+    socket.on('ca_1', ca_1);
+    function ca_1(data){
+        io.sockets.emit('r_ca_1');
+    }
+    // Canna 2
+    socket.on('ca_2', ca_2);
+    function ca_2(data){
+        io.sockets.emit('r_ca_2');
+    }
+    // Canna 3
+    socket.on('ca_3', ca_3);
+    function ca_3(data){
+        console.log('arrivato?');
+        io.sockets.emit('r_ca_3');
+    }
+    // Canna 4
+    socket.on('ca_4', ca_4);
+    function ca_4(data){
+        io.sockets.emit('r_ca_4');
+    }
+    // Canna 5
+    socket.on('ca_5', ca_5);
+    function ca_5(data){
+        io.sockets.emit('r_ca_5');
+    }
+    // Canna 6
+    socket.on('ca_6', ca_6);
+    function ca_6(data){
+        io.sockets.emit('r_ca_6');
+    }
+    // Canna 7
+    socket.on('ca_7', ca_7);
+    function ca_7(data){
+        io.sockets.emit('r_ca_7');
+    }
+    // Canna 8
+    socket.on('ca_8', ca_8);
+    function ca_8(data){
+        io.sockets.emit('r_ca_8');
+    }
+    
+    // Grilletto 1
+    socket.on('gr_1', gr_1);
+    function gr_1(data){
+        io.sockets.emit('r_gr_1');
+    }
+    // Grilletto 2
+    socket.on('gr_2', gr_2);
+    function gr_2(data){
+        io.sockets.emit('r_gr_2');
+    }
+    // Grilletto 3
+    socket.on('gr_3', gr_3);
+    function gr_3(data){
+        io.sockets.emit('r_gr_3');
+    }
+    // Grilletto 4
+    socket.on('gr_4', gr_4);
+    function gr_4(data){
+        io.sockets.emit('r_gr_4');
+    }
+    // Grilletto 5
+    socket.on('gr_5', gr_5);
+    function gr_5(data){
+        io.sockets.emit('r_gr_5');
+    }
+    // Grilletto 6
+    socket.on('gr_6', gr_6);
+    function gr_6(data){
+        io.sockets.emit('r_gr_6');
+    }
+    // Grilletto 7
+    socket.on('gr_7', gr_7);
+    function gr_7(data){
+        io.sockets.emit('r_gr_7');
+    }
+    // Grilletto 8
+    socket.on('gr_8', gr_8);
+    function gr_8(data){
+        io.sockets.emit('r_gr_8');
     }
 };
