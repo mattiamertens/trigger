d7076575d8e34309c19efdc68404f2bca8cf7f3d
Mattia Mertens <mattianc@hotmail.it>
2019-12-26 23:13:58 +0100

    loader

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(window).on('load', function(){
     console.log('daje!');
+    $('.load_wrapper').fadeOut();
     
 });
 
@@ -58,27 +59,27 @@ $(".finish").on('click', function(){
 
 
 // Video animation on scroll (ABOUT US)
-// var frameNumber = 0, // start video at frame 0
-//     // lower numbers = faster playback
-//     playbackConst = 240, 
-//     // get page height from video duration
-//     setHeight = document.getElementById("set-height"), 
-//     // select video element         
-//     vid = document.getElementById('animated_txt'); 
-//     // var vid = $('#v0')[0]; // jquery option
+var frameNumber = 0, // start video at frame 0
+    // lower numbers = faster playback
+    playbackConst = 240, 
+    // get page height from video duration
+    setHeight = document.getElementById("set-height"), 
+    // select video element         
+    vid = document.getElementById('animated_txt'); 
+    // var vid = $('#v0')[0]; // jquery option
 
 // dynamically set the page height according to video length
-// vid.addEventListener('loadedmetadata', function() {
-//   setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
-//   console.log('sticazzi');
-// });
+vid.addEventListener('loadedmetadata', function() {
+  setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+  console.log('sticazzi');
+});
 
 
 // Use requestAnimationFrame for smooth playback
-// function scrollPlay(){  
-//   var frameNumber  = window.pageYOffset/playbackConst;
-//   vid.currentTime  = frameNumber;
-//   window.requestAnimationFrame(scrollPlay);
-// }
+function scrollPlay(){  
+  var frameNumber  = window.pageYOffset/playbackConst;
+  vid.currentTime  = frameNumber;
+  window.requestAnimationFrame(scrollPlay);
+}
 
-// window.requestAnimationFrame(scrollPlay);
+window.requestAnimationFrame(scrollPlay);
