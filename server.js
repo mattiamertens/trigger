6c30b6b29c1dd1b3fb73aaa62882b203fa2c2b1d
Mattia Mertens <mattianc@hotmail.it>
2020-02-12 15:41:53 +0100

    xx

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ transporter.use('compile', hbs(handlebarOptions));
 // Socket.io projector
 const express = require('express');
 const app = express();
-// app.use(express.static('public'));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+// app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 const server = app.listen(process.env.PORT || 5000, function() {
