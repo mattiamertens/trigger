73cfb82cc4d71378b9b50534bb42a65484958584
Mattia Mertens <mattianc@hotmail.it>
2020-02-12 15:40:15 +0100

    app.use?

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ transporter.use('compile', hbs(handlebarOptions));
 // Socket.io projector
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 
 const server = app.listen(process.env.PORT || 5000, function() {
     const port = server.address().port;
@@ -42,7 +44,6 @@ const server = app.listen(process.env.PORT || 5000, function() {
 });
 
 // const server = app.listen(3000);
-// app.use('/public', express.static(path.join(__dirname, 'public')));
 // app.use(express.urlencoded({
 //     extended: false
 // }));
